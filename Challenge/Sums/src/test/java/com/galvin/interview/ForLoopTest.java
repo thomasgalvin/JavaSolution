679d109b2b672520d7ca4c71998f6f42ad3ded9b
Thomas Galvin <tgalvin@caci.com>
2018-04-24 09:43:57 -0400

    Refactored sums test

---
+++ Challenge/Sums/src/test/java/com/galvin/interview/ForLoopTest.java
@@ -0,0 +1,9 @@
+package com.galvin.interview;
+
+import org.junit.Test;
+
+public class ForLoopTest {
+    @Test public void testWhileLoop() throws Exception{
+        new SumsTestHarness().testWhileLoop();
+    }
+}
---
+++ Challenge/Sums/src/test/java/com/galvin/interview/WhileLoopTest.java
@@ -0,0 +1,9 @@
+package com.galvin.interview;
+
+import org.junit.Test;
+
+public class WhileLoopTest {
+    @Test public void testForLoop() throws Exception{
+        new SumsTestHarness().testForLoop();
+    }
+}
