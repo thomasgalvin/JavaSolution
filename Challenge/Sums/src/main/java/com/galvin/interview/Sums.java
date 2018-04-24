253c37458b2aee34d360e7e8ed6c79b4797253ac
Thomas Galvin <tgalvin@caci.com>
2018-04-24 14:44:04 -0400

    Added more documentation to the streams challenge

--- Challenge/Sums/src/main/java/com/galvin/interview/Sums.java
+++ Challenge/Sums/src/main/java/com/galvin/interview/Sums.java
@@ -6,9 +6,12 @@ import java.util.Arrays;
  * This program calculates the sum of the numbers in an array, but provides
  * four different methods of doing so; the first uses a for-loop, the second
  * a while-loop, the third uses recursion, and the fourth uses Java's new
- * Stream API
+ * Stream API.
  *
- * Implement all three methods.
+ * Implement all four methods.
+ *
+ * Documentation for the Stream API can be found here:
+ * https://docs.oracle.com/javase/8/docs/api/java/util/Arrays.html#stream-double:A-
  *
  * Hint: It may be easier to add a helper method for your recursion solution.
  */
